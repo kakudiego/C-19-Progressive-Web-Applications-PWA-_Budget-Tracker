bf852e9cfe42d07ebb95601d083a2a0d2714be9b
Diego Kaku <diegokaku@gmail.com>
2022-05-07 11:45:35 -0500

    idb.js error

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,16 +1,24 @@
+// create variable to hold db connection
 let db;
-const request = indexedDB.open('budget_tracker', 1);
+// establish a connection to IndexedDB database called 'pbudget_tracker' and set it to version 1
+const request = indexedDB.open("budget_tracker", 1);
 
+// this event will emit if the database version changes (nonexistant to version 1, v1 to v2, etc.)
 request.onupgradeneeded = function (event) {
+  // save a reference to the database
   const db = event.target.result;
-  db.createObjectStore('new_transaction', { autoIncrement: true });
+  // create an object store (table) called `new_transaction`, set it to have an auto incrementing primary key of sorts
+  db.createObjectStore("new_transaction", { autoIncrement: true });
 };
 
 request.onsuccess = function (event) {
+  // when db is successfully created with its object store (from onupgradedneeded event above) or simply established a connection, save reference to db in global variable
   db = event.target.result;
 
+  // check if app is online, if yes run uploadTransaction() function to send all local db data to api
   if (navigator.onLine) {
-    uploadBudget();
+    // we haven't created this yet, but we will soon, so let's comment it out for now
+    uploadTransaction();
   }
 };
 
@@ -18,33 +26,38 @@ request.onerror = function (event) {
   console.log(event.target.errorCode);
 };
 
+// This function will be executed if we attempt to submit a new expense and there's no internet connection
 function saveRecord(record) {
-  const transaction = db.transaction(['new_transaction'], 'readwrite');
+  // open a new transaction with the database with read and write permissions
+  const transaction = db.transaction(["new_transaction"], "readwrite");
 
-  const budgetObjectStore = transaction.objectStore('new_transaction');
+  // access the object store for `new_transaction`
+  const budgetObjectStore = transaction.objectStore("new_transaction");
 
+  // add record to your store with add method
   budgetObjectStore.add(record);
 }
 
-function uploadBudget() {
-  // open a transaction on your pending db
-  const transaction = db.transaction(['new_transaction'], 'readwrite');
+function uploadTransaction() {
+  // open a transaction on your db
+  const transaction = db.transaction(["new_transaction"], "readwrite");
 
-  // access your pending object store
-  const budgetObjectStore = transaction.objectStore('new_transaction');
+  // access your object store
+  const budgetObjectStore = transaction.objectStore("new_transaction");
 
   // get all records from store and set to a variable
   const getAll = budgetObjectStore.getAll();
 
+  // upon a successful .getAll() execution, run this function
   getAll.onsuccess = function () {
     // if there was data in indexedDb's store, let's send it to the api server
     if (getAll.result.length > 0) {
-      fetch('/api/pizzas', {
-        method: 'POST',
+      fetch("/api/transaction", {
+        method: "POST",
         body: JSON.stringify(getAll.result),
         headers: {
-          Accept: 'application/json, text/plain, */*',
-          'Content-Type': 'application/json',
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
         },
       })
         .then((response) => response.json())
@@ -52,14 +65,16 @@ function uploadBudget() {
           if (serverResponse.message) {
             throw new Error(serverResponse);
           }
-
-          const transaction = db.transaction(['new_transaction'], 'readwrite');
-          const budgetObjectStore = transaction.objectStore('new_transaction');
+          // open one more transaction
+          const transaction = db.transaction(["new_transaction"], "readwrite");
+          // access the new_transaction object store
+          const budgetObjectStore = transaction.objectStore("new_transaction");
           // clear all items in your store
           budgetObjectStore.clear();
+
+          alert("Transactions updated!");
         })
         .catch((err) => {
-          // set reference to redirect back here
           console.log(err);
         });
     }
@@ -67,4 +82,4 @@ function uploadBudget() {
 }
 
 // listen for app coming back online
-window.addEventListener('online', uploadBudget);
+window.addEventListener("online", uploadTransaction);
--- public/service-worker.js
+++ public/service-worker.js
@@ -18,16 +18,16 @@ const FILES_TO_CACHE = [
   './icons/icon-72x72.png'
 ]
 
-self.addEventListener('install', function (e) {
+self.addEventListener("install", function (e) {
   e.waitUntil(
     caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
+      console.log("installing cache : " + CACHE_NAME);
+      return cache.addAll(FILES_TO_CACHE);
     })
-  )
+  );
 });
 
-self.addEventListener('activate', function (e) {
+self.addEventListener("activate", function (e) {
   e.waitUntil(
     caches.keys().then(function (keyList) {
       let cacheKeeplist = keyList.filter(function (key) {
@@ -35,27 +35,29 @@ self.addEventListener('activate', function (e) {
       });
       cacheKeeplist.push(CACHE_NAME);
 
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i]);
-          return caches.delete(keyList[i]);
-        }
-      }));
+      return Promise.all(
+        keyList.map(function (key, i) {
+          if (cacheKeeplist.indexOf(key) === -1) {
+            console.log("deleting cache : " + keyList[i]);
+            return caches.delete(keyList[i]);
+          }
+        })
+      );
     })
-  )
+  );
 });
 
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request : " + e.request.url);
   e.respondWith(
     caches.match(e.request).then(function (request) {
       if (request) {
-        console.log('responding with cache : ' + e.request.url)
-        return request
+        console.log("responding with cache : " + e.request.url);
+        return request;
       } else {
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
+        console.log("file is not cached, fetching : " + e.request.url);
+        return fetch(e.request);
       }
     })
-  )
+  );
 });
